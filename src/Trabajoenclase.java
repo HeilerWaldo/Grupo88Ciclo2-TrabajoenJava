a66a0d954289b61bd4aa1a7144599bba943e2311
HeilerWaldo <heiwal10@hotmail.com>
2021-07-05 22:54:36 -0500

    Java lang lesson

---
+++ src/Trabajoenclase.java
@@ -0,0 +1,79 @@
+public class App {
+
+    private String nombre;
+    private int identificacion;
+    private double estatura;
+    private float peso;
+    private char sexo;
+    private boolean estaVacunado;
+
+    public static void main2(String[] args) {
+        System.out.println("Hello, World!");
+        //operadores aritmeticos
+        int resultado = 45 + 12;
+        resultado = 45 - 12;
+        resultado = 45 * 12;
+        resultado = 45 / 12;
+        resultado = 45 % 12;
+         //operadores unarios
+         respuesta++;
+         resultado = respuesta--;
+         //!resultado
+        boolean respuesta = true;
+        //algebra booleana
+        respuesta = 5 < 8
+        respuesta = 5 > 8;
+        respuesta = 5 >= 8;
+        respuesta = 5 <= 8;
+        respuesta = 5 == 8;
+        respuesta = 5 != 8;
+        //operadores logicos
+        respuesta = 5 < 8 && 4 >= 6;
+        respuesta = 5 < 8 || 4 >= 6;
+        //operadores bit a bit
+        // &
+        // |
+        // ^
+        // estructuras de control
+        //---condicionales---
+        if (5 != 10) {
+            //bloqueo de código
+        } 
+
+        if (respuesta) {
+            //bloqueo de código
+        } else if(!respuesta){
+            //bloqueo de código
+        } else {
+            //bloqueo de código
+        }
+        int llave = 2;
+        switch (llave) {
+            case 1:
+                //bloqueo de código
+                break;
+            case 2:
+                //bloqueo de código
+                break;
+        
+            default:
+                break;
+        }
+        //---ciclos, repetitivas, bucles---
+        while (llave > 5) {
+            //bloqueo de código
+        }
+
+        do {
+           //bloqueo de código 
+        } while (llave > 5);
+
+        for (String string : args) {
+            //bloque de código
+        }
+        //declaración de una variable ; condición ; incremento
+        for (int i = 0; i < args.length; i++) {
+            
+        }
+    }
+}
